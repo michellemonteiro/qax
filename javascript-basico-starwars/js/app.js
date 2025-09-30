b114eeff1e84e183b09f5c16c37ad44fc5979a10
Michelle Monteiro <michelle.monteiro.ma@gmail.com>
2025-09-30 19:51:40 -0300

    Conclusão do curso JS, CSS e HTML ❤︎

--- javascript-basico-starwars/js/app.js
+++ javascript-basico-starwars/js/app.js
@@ -4,37 +4,37 @@ const LIST = [{
     avatar: 'images/c3po.png'
 },
 {
-    id: 1,
+    id: 2,
     nome: 'Chewbacca',
     avatar: 'images/chewbacca.png'
 },
 {
-    id: 1,
+    id: 3,
     nome: 'Han Solo',
     avatar: 'images/hansolo.png'
 },
 {
-    id: 1,
+    id: 4,
     nome: 'Princesa Leia',
     avatar: 'images/leia.png'
 },
 {
-    id: 1,
+    id: 5,
     nome: 'Luke Skywalker',
     avatar: 'images/luke.png'
 },
 {
-    id: 1,
+    id: 6,
     nome: 'R2D2',
     avatar: 'images/r2d2.png'
 },
 {
-    id: 1,
+    id: 7,
     nome: 'Darth Vader',
     avatar: 'images/vader.png'
 },
 {
-    id: 1,
+    id: 8,
     nome: 'Mestre Yoda',
     avatar: 'images/yoda.png'
 }
@@ -46,11 +46,40 @@ const App = new Vue({
     data: {
         title: 'Statr Wars Lego',
         userName: 'Michelle',
-        characters: LIST
+        characters: LIST,
+        searchName: ''
     },
     methods: {
         like(userName) {
             alert(`O personagem ${userName} recebeu um like!`)
+        },
+        remove(id) {
+            const list = this.characters
+
+            const result = list.filter(item => {
+                return item.id !== id
+            })
+
+            this.characters = result
+        },
+        search() {
+
+            if (this.searchName === '') {
+               return  alert('O campo de busca é obrigatório!')
+            }
+            const list = this.characters = LIST
+
+            const result = list.filter(item => {
+                return item.nome === this.searchName
+
+            })
+            if (result.length <= 0) {
+                alert('Nenhum registro encontrado.')
+            } else {
+                this.characters = result
+            }
+
+            
         }
     }
 })
